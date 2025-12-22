dd56ae691769c1fd22bf6795d2922e264d49d442
Mgharabli <mhtradingeu@gmail.com>
2025-12-22 14:40:32 +0100

    test: all suites passing, phase stabilized

--- src/models/index.js
+++ src/models/index.js
@@ -1,57 +1,29 @@
 const { sequelize, Sequelize } = require('../lib/database');
-const defineUser = require('./User');
-const defineCompany = require('./Company');
-const defineInvoice = require('./Invoice');
-const defineTransaction = require('./Transaction');
-const defineTaxReport = require('./TaxReport');
-const defineBankStatement = require('./BankStatement');
-const defineBankTransaction = require('./BankTransaction');
 
-const defineInvoiceItem = require('./InvoiceItem');
-const defineExpense = require('./Expense');
-const defineAuditLog = require('./AuditLog');
-const defineFileAttachment = require('./FileAttachment');
-const defineAIInsight = require('./AIInsight');
-const defineAIInsightDecision = require('./AIInsightDecision');
-const defineRevokedToken = require('./RevokedToken');
-const defineActiveToken = require('./ActiveToken');
+const modelDefiners = [
+  require('./User'),
+  require('./Company'),
+  require('./Invoice'),
+  require('./InvoiceItem'),
+  require('./Expense'),
+  require('./FileAttachment'),
+  require('./BankStatement'),
+  require('./BankTransaction'),
+  require('./Transaction'),
+  require('./TaxReport'),
+  require('./AuditLog'),
+  require('./AIInsight'),
+  require('./AIInsightDecision'),
+  require('./ActiveToken'),
+  require('./RevokedToken'),
+];
 
-const User = defineUser(sequelize);
-const Company = defineCompany(sequelize);
-const Invoice = defineInvoice(sequelize);
-const Transaction = defineTransaction(sequelize, Sequelize.DataTypes);
-const TaxReport = defineTaxReport(sequelize, Sequelize.DataTypes);
-const BankStatement = defineBankStatement(sequelize, Sequelize.DataTypes);
-const BankTransaction = defineBankTransaction(sequelize, Sequelize.DataTypes);
+const models = {};
 
-
-const InvoiceItem = defineInvoiceItem(sequelize);
-const Expense = defineExpense(sequelize);
-const AuditLog = defineAuditLog(sequelize, Sequelize.DataTypes);
-const FileAttachment = defineFileAttachment(sequelize, Sequelize.DataTypes);
-const AIInsight = defineAIInsight(sequelize);
-const AIInsightDecision = defineAIInsightDecision(sequelize);
-const RevokedToken = defineRevokedToken(sequelize, Sequelize.DataTypes);
-const ActiveToken = defineActiveToken(sequelize, Sequelize.DataTypes);
-
-
-const models = {
-  User,
-  Company,
-  Invoice,
-  InvoiceItem,
-  Expense,
-  Transaction,
-  TaxReport,
-  BankStatement,
-  BankTransaction,
-  AuditLog,
-  FileAttachment,
-  AIInsight,
-  AIInsightDecision,
-  RevokedToken,
-  ActiveToken,
-};
+modelDefiners.forEach((defineModel) => {
+  const model = defineModel(sequelize, Sequelize.DataTypes);
+  models[model.name] = model;
+});
 
 Object.values(models).forEach((model) => {
   if (typeof model.associate === 'function') {
--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,19 +1,4 @@
-
-process.env.NODE_ENV = 'test';
-process.env.DATABASE_URL = 'sqlite::memory:';
-const { sequelize } = require('../src/models');
-
 module.exports = async () => {
-  // Set test environment
-  process.env.DB_NAME = 'smartaccounting_test';
-  
-  // Create test database
-  try {
-    await sequelize.authenticate();
-    await sequelize.sync({ force: true });
-    console.log('✅ Test database setup complete');
-  } catch (error) {
-    console.error('❌ Test database setup failed:', error);
-    process.exit(1);
-  }
+  // CI / Jest bootstrap: do not initialize Sequelize models or hit the database.
+  process.env.NODE_ENV = 'test';
 };
