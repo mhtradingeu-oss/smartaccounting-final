ba76d1b46427095081c2863b1818abe5d75ae33c
Mgharabli <mhtradingeu@gmail.com>
2025-12-23 12:11:20 +0100

    ci: disable globalSetup to prevent DB bootstrap in CI

--- jest.config.js
+++ jest.config.js
@@ -2,31 +2,18 @@ module.exports = {
   testEnvironment: 'node',
   setupFiles: ['./tests/env.js'],
   setupFilesAfterEnv: ['./tests/setup.js'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov', 'html'],
-  collectCoverageFrom: [
-    'src/**/*.js',
-    '!src/config/**',
-    '!src/templates/**',
-    '!**/node_modules/**',
-    '!**/coverage/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
-    },
-  },
+
+  collectCoverage: false,
+
   testMatch: ['**/tests/**/*.test.js', '**/__tests__/**/*.test.js'],
+
   verbose: true,
   forceExit: true,
   clearMocks: true,
   restoreMocks: true,
   testTimeout: 30000,
-  //globalSetup: './tests/globalSetup.js',
-  //globalTeardown: './tests/globalTeardown.js',
-  globalSetup: process.env.CI ? undefined : './tests/globalSetup.js',
+
+  // 🚫 NEVER run DB bootstrap in CI
+  globalSetup: undefined,
+  globalTeardown: undefined,
 };
